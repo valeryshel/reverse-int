8afd317652ea1441fd2f031c9271958f2ca28c53
valeryshel <valeryshel@gmail.com>
2021-02-16 16:40:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 module.exports = function reverse (n) {
-    let a = n < 0 ? n*(-1): n;
+    let a = n < 0 ? n*(-1): n; // если число <0 , то умножаем на -1
     a = a.toString().split('').reverse().join('');
+    return a;
 }
